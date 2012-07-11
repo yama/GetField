c82843fc7d2c4ea4f09d9f476dcd32bb3b57b035
yama <yamamoto@kyms.ne.jp>
2012-07-11 18:01:45 +0900

    First code (1.3 beta)

---
+++ install.php
@@ -0,0 +1,13 @@
+<?php 
+$resource_type = 'snippet';
+$resource_name = 'GetField';
+$resource_desc = 'Returns any document field or template variable from any document or any of its parents.';
+$resource_file = 'GetField.tpl';
+$resource_version = '1.3 beta';
+$resource_help = 'GetField.txt';
+$resource_license = 'LGPL';
+$resource_events = array();
+$resource_config = '';
+echo '<h1 class="modxpkg">This is the installation file for '.$resource_name.'.</h1>'."\n";
+echo '<h2 class="modxpkg">To install, please use the MODx Resource Wizard.</h2>'."\n";
+?> 
